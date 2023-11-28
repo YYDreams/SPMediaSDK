dc28c40e6f170f624fd0a2d97373e70d193334b2
flowerflower <799431668@qq.com>
2023-11-28 16:29:22 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-SPMediaSDK_Tests/Pods-SPMediaSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPMediaSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SPMediaSDK_Tests
+@end
---
+++ Example/Pods/Target Support Files/SPMediaSDK/SPMediaSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SPMediaSDK : NSObject
+@end
+@implementation PodsDummy_SPMediaSDK
+@end
